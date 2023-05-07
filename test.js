35ca0d219b5e1f3ad6a882a905ea80c153f83175
harutoreon <norabder567@gmail.com>
2023-05-08 05:07:17 +0900

    Add rules of keyword-spacing

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 function hello(name) {
-  document.body.textContent = 'Hello, ' + name + '!';
+  if (!name) {
+    return 'hello';
+  } else {
+    document.body.textContent = 'Hello, ' + name + '!';
+  }
 }
 
 hello('World');  // Hello, World!
